419930e5bb1bdfaa319fdb76a1a9cad75c2c132c
Topi <topi@topi-desktop.(none)>
2013-08-21 08:49:59 +0300

    kommentin pohja lisätty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130811141851) do
+ActiveRecord::Schema.define(version: 20130817094048) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recipes", force: true do |t|
     t.string   "name"
